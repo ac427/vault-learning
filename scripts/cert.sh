229210e8063d6b18e15db6bdc0145db5b27f966a
Ananta Chakravartula <ananta.c@gmail.com>
2020-10-03 07:31:53 -0400

    fixed cert path

--- scripts/cert.sh
+++ scripts/cert.sh
@@ -10,7 +10,7 @@ openssl req -nodes -newkey rsa:2048 -keyout server.key -out server.csr -subj "/C
 openssl x509 -req -in server.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out server.crt
 
 # Create server PEM file
-cat server.key server.crt > server.pem
+cat server.key server.crt > ../cert/server.pem
 
 
 # Generate client cert to be signed
@@ -20,4 +20,4 @@ openssl req -nodes -newkey rsa:2048 -keyout client.key -out client.csr -subj "/C
 openssl x509 -req -in client.csr -CA ca.crt -CAkey ca.key -CAserial ca.srl -out client.crt
 
 # Create client PEM file
-cat client.key client.crt > client.pem
+cat client.key client.crt > ../cert/client.pem
